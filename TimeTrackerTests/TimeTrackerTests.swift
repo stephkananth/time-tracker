9890569016ff5cf7d189183fb2d75cd61728b7df
Steph Ananth <steph.ananth@gmail.com>
2020-07-06 15:52:21 -0400

    fix style issues pt. 4 (trailing whitespace)

--- TimeTrackerTests/TimeTrackerTests.swift
+++ TimeTrackerTests/TimeTrackerTests.swift
@@ -10,7 +10,6 @@ import XCTest
 @testable import TimeTracker
 
 class TimeTrackerTests: XCTestCase {
-
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
