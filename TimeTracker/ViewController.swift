eabf5eb1b1edd0ee9f74aa32ed43e1f2405fe773
Steph Ananth <steph.ananth@gmail.com>
2020-07-06 15:19:34 -0400

    initial commit

---
+++ TimeTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TimeTracker
+//
+//  Created by Steph Ananth on 7/6/20.
+//  Copyright © 2020 Steph K. Ananth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
